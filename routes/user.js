a8f9ed96cb9ca762093aebb3119468544bb2cda2
njine-hub <maggienjine340@gmail.com>
2025-11-21 04:45:38 +0300

    Remove deleted files

--- routes/user.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { reportIncident, getUserIncidents } = require("../controllers/userController");
-const auth = require("../middleware/authMiddleware");
-router.post("/report", auth, reportIncident);
-router.get("/incidents", auth, getUserIncidents);
-module.exports = router;
